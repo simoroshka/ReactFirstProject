8fb5e31d94e2d6e15cbcab4c0a16dd2706b3948e
Anna Kruglaia <simoroshka@gmail.com>
2017-01-28 19:30:17 +0200

    Add SASS support

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,7 @@ class SearchBar extends Component {
 
   render () {
     return (
-      <div className="search-bar">
+      <div className="search-bar col-md-8">
         <input
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
