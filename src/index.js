e282c4cadf18fb60e2fc159c77db438cd61bae02
Anna Kruglaia <simoroshka@gmail.com>
2017-01-28 17:55:10 +0200

    Make the search work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -20,18 +21,24 @@ class App extends Component {
       selectedVideo: null,
     };
 
-    YTSearch({key: API_KEY, term: 'kitties'}, (videos) => {
+    this.videoSearch('kitties');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0],
       });
-
     });
   }
+
   render() {
+    // the function is possible to run only every 300ms
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
