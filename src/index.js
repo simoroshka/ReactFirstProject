9c97144f7c72edd59277e03da937ac1d36eb3212
Anna Kruglaia <simoroshka@gmail.com>
2017-01-28 13:45:59 +0200

    Prepare the project to use youtube API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Remember to restrict the key usage to the domain!
+const APY_KEY = 'AIzaSyBD-4BfWdw6Xc3pQJ_ITr5y92vb_iTkC-I';
+
 const App = function() {
   return <div>Hello world!</div>; //JSX stuff
 }
