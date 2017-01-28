0815c80c0e41dca2e55e4f4d7c934c0fcda6e8a6
Anna Kruglaia <simoroshka@gmail.com>
2017-01-28 16:06:01 +0200

    Refactor App, test the video search

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 // Remember to restrict the key usage to the domain!
-const APY_KEY = 'AIzaSyBD-4BfWdw6Xc3pQJ_ITr5y92vb_iTkC-I';
+// const API_KEY = 'AIzaSyBD-4BfWdw6Xc3pQJ_ITr5y92vb_iTkC-I';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+// server key for development, restriction based on IP
+const API_KEY = 'AIzaSyAQuIZXtIxO4sGtTJckvQzal38TBMRHUFw';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'kitties'}, (videos) => {
+      this.setState({ videos });
+      console.log(videos);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
